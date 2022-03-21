0a5a48284e2df4cac56952185ad769f12cae9e8d
smile <>
2022-03-21 16:45:54 +0800

    样式

--- config/constant.js
+++ config/constant.js
@@ -1,7 +1,8 @@
 //const BASE_URL = 'http://42.192.146.116:9052'
-// const BASE_URL = 'http://42.192.146.116:9050/mall-miniapp'
+const BASE_URL = 'http://42.192.146.116:9050/mall-miniapp'
+// const BASE_URL = 'https://www.moyangbook.com/mall-miniapp'
 // const BASE_URL = 'http://192.168.5.193:9050/mall-miniapp'
-const BASE_URL = 'https://www.moyangbook.com/mall-miniapp'
+// const BASE_URL = 'https://www.moyangbook.com/mall-miniapp'
 
 module.exports = {
   BASE_URL
